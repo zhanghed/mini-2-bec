6d143825a5fed5e32a5c67f9b626138968efb25f
zhanghed <747988598@qq.com>
2024-01-31 15:33:39 +0800

    w

--- miniprogram/pages/set/set.js
+++ miniprogram/pages/set/set.js
@@ -1,30 +1,271 @@
+// 全局变量
+const App = getApp().globalData
+// 颜色对象集合
+const colors = [{
+		name: '休息',
+		color: 'xiuxi'
+	},
+	{
+		name: '全天',
+		color: 'quantian'
+	},
+	{
+		name: '白班',
+		color: 'baiban'
+	},
+	{
+		name: '夜班',
+		color: 'yeban'
+	},
+	{
+		name: '早班',
+		color: 'zaoban'
+	},
+	{
+		name: '中班',
+		color: 'zhongban'
+	},
+	{
+		name: '晚班',
+		color: 'wanban'
+	},
+	{
+		name: '大夜',
+		color: 'daye'
+	},
+	{
+		name: '小夜',
+		color: 'xiaoye'
+	},
+	{
+		name: '大白',
+		color: 'dabai'
+	},
+	{
+		name: '小白',
+		color: 'xiaobai'
+	},
+	{
+		name: '值班',
+		color: 'zhiban'
+	},
+]
+// 规则选择器集合
+const range = function () {
+	let temp = colors.map(i => i.name)
+	temp.push('自定义')
+	return temp
+}
 Page({
+	data: {
+		range: range(),
+		// dis: App.dis,
+		dis: [{
+			"listDate": "2024/1/30",
+			"listName": "dfa",
+			"listRule": [
+				"休息",
+				"饭干饭",
+				"休息",
+				"全天",
+				"白班",
+				"休息",
+				"休息",
+				"休息",
+				"全天",
+				"白班",
+				"休息",
+				"饭干饭",
+				"休息",
+				"全天",
+				"白班",
+				"休息",
+				"休息",
+				"休息",
+				"全天",
+				"白班",
+				"休息",
+				"饭干饭",
+				"休息",
+				"全天",
+				"白班",
+				"休息",
+				"休息",
+				"休息",
+				"全天",
+				"白班",
+				"休息",
+				"饭干饭",
+				"休息",
+				"全天",
+				"白班",
+				"休息",
+				"休息",
+				"休息",
+				"全天",
+				"白班",
+				"休息",
+				"饭干饭",
+				"休息",
+				"全天",
+				"白班",
+				"休息",
+				"休息",
+				"休息",
+				"全天",
 
-  data: {
-    array: ['美国', '中国', '巴西', '日本'],
-    no: 0,
-    dis: [{
-      listName: '',
-      listDate: '',
-      listRule: []
-    }]
-  },
-  // 输入
-  bindKeyInput: function (e) {
-    console.log(e.detail.value)
-  },
-  // 日期
-  bindDateChange: function (e) {
-    console.log(e.detail.value)
-  },
-  // 规则
-  bindPickerChange: function (e) {
-    console.log(e.detail.value)
-    // let dis = this.data.dis
-    // dis[this.data.no].listRule.push(this.data.array[e.detail.value])
-    // this.setData({
-    //   dis: dis
-    // })
-  },
+			]
+		}, {
+			"listDate": "2024/1/30",
+			"listName": "未命名1",
+			"listRule": [
+				"休息",
+				"白班"
+			]
+		}, {
+			"listDate": "2024/1/8",
+			"listName": "dfa2",
+			"listRule": [
+				"休息",
+				"饭干饭",
+				"休息",
+				"全天",
+				"白班",
+				"休息",
+				"休息",
+				"休息",
+				"全天",
+				"白班",
+				"休息",
+				"饭干饭",
+				"休息",
+				"全天",
+				"白班",
+				"休息",
+				"休息",
+				"休息",
+				"全天",
+				"白班",
+				"休息",
+				"饭干饭",
+				"休息",
+				"全天",
+				"白班",
+				"休息",
+				"休息",
+				"休息",
+				"全天",
+				"白班",
+				"休息",
+				"饭干饭",
+				"休息",
+				"全天",
+				"白班",
+				"休息",
+				"休息",
+				"休息",
+				"全天",
+				"白班",
+				"休息",
+				"饭干饭",
+				"休息",
+				"全天",
+				"白班",
+				"休息",
+				"休息",
+				"休息",
+				"全天",
 
+			]
+		}, {
+			"listDate": "2024/1/30",
+			"listName": "未命名2",
+			"listRule": [
+				"休息",
+				"白班"
+			]
+		}, {
+			"listDate": "2024/1/30",
+			"listName": "未命名3",
+			"listRule": [
+				"休息",
+				"白班"
+			]
+		}],
+		show: []
+	},
+	// 显示元素结构
+	showFun: function (item) {
+		let temp = colors.find(i => i.name === item)
+		return temp || {
+			name: item,
+			color: 'zidingyi'
+		}
+	},
+	// 修改规则名称
+	listNameFun: function (event) {
+		let no = event.target.dataset.pid
+		let name = event.detail.value.trim() ? event.detail.value.trim().slice(0, 3) : '未命名'
+		let dis = this.data.dis
+		let show = this.data.show
+		dis[no].listName = name
+		show[no].listName = name
+		this.setData({
+			dis,
+			show
+		})
+	},
+	// 修改开始日期
+	listDateFun: function (event) {
+		let no = event.target.dataset.pid
+		let date = event.detail.value.replaceAll("-", "/")
+		let dis = this.data.dis
+		let show = this.data.show
+		dis[no].listDate = date
+		show[no].listDate = date
+		this.setData({
+			dis,
+			show
+		})
+	},
+	// 追加规则
+	listRuleFun: function (event) {
+		let no = event.target.dataset.pid
+		let rangeNo = event.detail.value
+		let dis = this.data.dis
+		let show = this.data.show
+		dis[no].listRule.push(this.data.range[rangeNo])
+		show[no].listRule.push(this.showFun(this.data.range[rangeNo]))
+		this.setData({
+			dis,
+			show
+		})
+	},
+	// 删除规则
+	listRulePopFun: function (event) {
+		let no = event.target.dataset.pid
+		console.log(event)
+		let dis = this.data.dis
+		let show = this.data.show
+		dis[no].listRule.pop()
+		show[no].listRule.pop()
+		this.setData({
+			dis,
+			show
+		})
+	},
+	// 加载
+	onLoad: function () {
+		// 深拷贝
+		let dis = JSON.parse(JSON.stringify(this.data.dis))
+		// 构建显示数据
+		for (let index = 0; index < dis.length; index++) {
+			dis[index].listRule = dis[index].listRule.map(item => {
+				return this.showFun(item)
+			})
+		}
+		this.setData({
+			show: dis
+		})
+	},
 })
